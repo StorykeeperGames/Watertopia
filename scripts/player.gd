58e39c49a2edf02c5fc502d4048f4112171e5ae2
Ryan May <imaycode@gmail.com>
2026-07-19 13:22:03 -0500

    feat: add movement and springarm

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,37 @@
 extends CharacterBody3D
 
 
+@onready var camera_pivot: Node3D = $CameraPivot
+
+@export var mouse_sensitivity: float = 0.003
+@export var min_pitch: float = -50.0 # Lowest look angle in degrees
+@export var max_pitch: float =  50.0 # Highest look angle in degrees
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		# Rotate character horizontally (yaw)
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		
+		# Rotate pivot vertically (pitch)
+		camera_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera_pivot.rotation.x = clamp(
+			camera_pivot.rotation.x, 
+			deg_to_rad(min_pitch), 
+			deg_to_rad(max_pitch)
+		)
+		
+	# Press UI cancel (usually Escape) to release the mouse cursor
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +44,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
